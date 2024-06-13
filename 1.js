e0489c80bfae121eac03b330d613f39cd4ed5af4
mosimoe <mosi88866686@gmail.com>
2024-06-13 16:05:38 +0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,9 @@
 /******************************
 
-
-*******************************
-
-
 [rewrite_local]
 
 
-^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/gx/bilibili.js
+^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/mosimoe/1/main/1.js
 
 [mitm]
 hostname = *.biliapi.*, *.bilibili.*
@@ -17,6 +13,9 @@ hostname = *.biliapi.*, *.bilibili.*
 var modifiedHeaders=$request['headers'];
 modifiedHeaders['Referer']='https://www.bilibili.com/';
 modifiedHeaders['User-Agent']='bili-universal/71400100 os/ios model/iPhone 11 mobi_app/iphone osVer/15.3.1 network/2';
+modifiedHeaders['Forwarded']='192.168.1.1';
+modifiedHeaders['X-Forwarded-For']='192.168.1.1';
+modifiedHeaders['X-Real-IP']='192.168.1.1';
 modifiedHeaders['buvid']='1234567890abcdef';
 modifiedHeaders['x-bili-locale-bin']='';
 modifiedHeaders['x-bili-metadata-bin']='';
